e9962c8cc34070c0a9efd5916ed0fb12226a06b5
Tyler Schloesser <tschloes@amazon.com>
2021-09-26 09:22:19 -0700

    add canvas

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,21 @@
 import ReactDom from 'react-dom'
 import { App } from './app'
 
+const beacons: Element[] = []
+
+const mo = new MutationObserver((records) => {
+  console.log(records)
+
+  records.forEach((record) => {
+    record.addedNodes.forEach((node) => {
+      console.log({ node })
+      ;(node as HTMLElement)
+        .querySelectorAll('[data-beacon]')
+        .forEach((beacon) => {
+          beacons.push(beacon)
+        })
+    })
+  })
+}).observe(document.getElementById('app')!, { childList: true, subtree: true })
+
 ReactDom.render(<App />, document.getElementById('app'))
